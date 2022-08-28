4bf3c66b573a20c7a70430aa5cf29271df4fac39
HOME <jeonghj27@naver.com>
2022-08-28 21:59:23 +0900

    22/08/28

---
+++ src/ex5/Exercise5_10.java
@@ -0,0 +1,33 @@
+package ex5;
+
+public class Exercise5_10 {
+
+	public static void main(String[] args) {
+		// 암호화하는 프로그램
+		char[] abcCode = 
+			{ '`','~','!','@','#','$','%','^','&','*',
+					'(',')','-','_','+','=','|','[',']','{',
+					'}',';',':',',','.','/'};
+		char[] numCode = {'q','w','e','r','t','y','u','i','o','p'}; //012346789
+		
+		String src = "abc123";
+		String result = "";
+		
+		for(int i=0; i<src.length(); i++) {
+			char ch = src.charAt(i);
+			
+			if('a' <= ch && ch <='z') {
+				result += abcCode[ch-'a'];
+			} else if('0' <= ch && ch <='9') {
+				result += numCode[ch-'0'];
+			}
+			
+		}
+		
+		System.out.println("scr : " + src);
+		System.out.println("result : " + result);
+		
+	
+	}
+
+}
